df3056303cb0cf829e438e7e4b8914b3027312b3
Guypatc <guypstc@gmail.com>
2024-08-27 15:47:15 +0700

    first code

---
+++ src/components/Banner.js
@@ -0,0 +1,78 @@
+import { useState, useEffect } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import HeadderImg from "../assets/image/Headder.png";
+
+export const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const toRotate = ["Web Developer", "Web Designer"];
+  const [text, setText] = useState("");
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const period = 2000;
+
+  useEffect(() => {
+    const ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => clearInterval(ticker);
+  }, [text, delta]);
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta((prevDelta) => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setDelta(period);
+    } else if (isDeleting && updatedText === "") {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setDelta(500);
+    }
+  };
+
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="align-item-center">
+          <Col xs={12} md={6} xl={7}>
+            <span className="tagline">Welcome to my Portfolio</span>
+            <h1>{`Hi I'm Prasittichai `}</h1>
+            <h1>
+              <span className="text-rotate">
+                <span className="wrap">{text}</span>
+              </span>
+            </h1>
+            <p>
+              I am interested in Web Developer. I am
+              seeking a challenging role at an organization where I can apply my
+              programming skills to contribute effectively. I am ready to
+              continuously improve myself.
+            </p>
+            <button onClick={() => console.log("connect")}>
+              Let's Connect <ArrowRightCircle size={25} />
+            </button>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <img
+              src={HeadderImg}
+              className="responsive-img"
+              alt="Headder Img"
+            />
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
---
+++ src/components/Navbar.js
@@ -0,0 +1,60 @@
+import { useState, useEffect } from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+/* รูป (Image) */
+import Logo from "../assets/image/Logo-Web.png";
+import navIcon1 from "../assets/icon/icon-facebook.png";
+import navIcon2 from "../assets/icon/icon-instagram.png";
+import navIcon3 from "../assets/icon/icon-tiktok.png"; 
+
+
+const MyNavbar = () => {
+    const [activelink, setActiveLink] = useState('home');
+    const [scrolled, setScolled] = useState(false);
+
+    useEffect(() => {
+        const onScroll = () => {
+            if(window.scrollY > 50){
+                setScolled(true);
+            }else{
+                setScolled(false);
+            }
+        }
+        window.addEventListener("scroll",onScroll);
+        return () => window.removeEventListener("scroll",onScroll);
+    }, [])
+
+    const onUpdateActiveLink = (value) => {
+        setActiveLink(value);
+    } 
+  return (
+    <Navbar expand="lg" className={scrolled ? "scrolled": ""}>
+      <Container>
+      <Navbar.Brand href="#home" className={activelink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>
+        <img src={Logo} width="50px" height="50px" alt="Logo" />
+      </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+            <span className="navbar-toggler-icon"></span>
+        </Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home" className={activelink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+            <Nav.Link href="#skills" className={activelink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
+            <Nav.Link href="#projects" className={activelink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+          </Nav>
+          <span className="navbar-text">
+            <div className="social-icon">
+                <a href="https://www.facebook.com/prasittichai.meaungthai/"><img src={navIcon1} alt="" /></a>
+                <a href="https://www.instagram.com/pstchai/?next=%2F"><img src={navIcon2} alt="" /></a>
+                <a href="https://www.tiktok.com/@guy_pstc?lang=en"><img src={navIcon3} alt="" /></a>
+            </div>
+            <button className="vvd" onClick={() => console.log('connect')}><span>Let's Connect</span></button>
+          </span>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default MyNavbar;
