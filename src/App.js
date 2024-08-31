3851589e01d302db32fcb068666289a1b6b8da6d
Guypatc <guypstc@gmail.com>
2024-08-31 21:05:24 +0700

    update:skills

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ function App() {
       <Banner />
       <Skills />
       <Project />
-  
     </div>
   );
 }
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,68 +1,56 @@
-import { Container, Row, Col } from "react-bootstrap";
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
-import Frontend_img from '../assets/image/Frontend_img.png';
-import Backend_img from '../assets/image/Backend_img.png';
-import Database_img from '../assets/image/Database_img.png';
-import Program_img from '../assets/image/Programing_img.png';
-import Framework_img from '../assets/image/Framework_img.png';
+import { Container, Row, Col, Card } from "react-bootstrap";
+
+//Skill img
+import img_css from '../assets/image/css.png';
+import img_html from '../assets/image/html.png';
+import img_js from '../assets/image/js.png';
+import img_react from '../assets/image/react.png';
+import img_boots from '../assets/image/bootstrap.png';
+import img_php from '../assets/image/php.png';
+import img_node from '../assets/image/node.png';
+import img_sql from '../assets/image/sql.png';
+import img_c from '../assets/image/c++.png';
+import img_java from '../assets/image/java.png';
 
 export const Skills = () => {
-    const responsive = {
-        superLargeDesktop: {
-          // the naming can be any, depends on you.
-          breakpoint: { max: 4000, min: 3000 },
-          items: 5
-        },
-        desktop: {
-          breakpoint: { max: 3000, min: 1024 },
-          items: 3
-        },
-        tablet: {
-          breakpoint: { max: 1024, min: 464 },
-          items: 2
-        },
-        mobile: {
-          breakpoint: { max: 464, min: 0 },
-          items: 1
-        }
-      };
-    return(
+    const skills = [
+        { img: img_css, title: "CSS", des: "Advance" },
+        { img: img_html, title: "HTML" },
+        { img: img_js, title: "Javascript" },
+        { img: img_react, title: "React" },
+        { img: img_boots, title: "Bootstrap" },
+        { img: img_php, title: "PHP" },
+        { img: img_node, title: "Node.js" },
+        { img: img_sql, title: "SQL" },
+        { img: img_c, title: "C++" },
+        { img: img_java, title: "Java" }
+    ];
+
+    return (
         <section className="skill" id="skills">
             <Container>
                 <Row>
                     <Col>
                         <div className="skill-bx">
-                            <h2>
-                                Skills
-                            </h2>
-                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit non distinctio quae fugit qui eligendi neque, ullam dolore eaque labore sit cum necessitatibus, id quo itaque odio quos? Adipisci, dolorem.</p>
-                            <Carousel responsive={responsive} infinite={true} autoPlay={true} autoPlaySpeed={5000} className="skill-slider">
-                                <div className="item">
-                                    <img src={Frontend_img} alt="Image" />
-                                    <h5>FrontEnd</h5>
-                                </div>
-                                <div className="item">
-                                    <img src={Backend_img} alt="Image" />
-                                    <h5>BackEnd</h5>
-                                </div>
-                                <div className="item">
-                                    <img src={Database_img} alt="Image" />
-                                    <h5>Database</h5>
-                                </div>
-                                <div className="item">
-                                    <img src={Framework_img} alt="Image" />
-                                    <h5>Framework</h5>
-                                </div>
-                                <div className="item">
-                                    <img src={Program_img} alt="Image" />
-                                    <h5>Programing</h5>
-                                </div>
-                            </Carousel>
+                            <h2>Skills</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit non distinctio quae fugit qui eligendi neque, ullam dolore eaque labore sit cum necessitatibus.</p>
+                            <Row className="skills-grid"> 
+                                {skills.map((skill, index) => (
+                                    <Col key={index}  className="mb-4">
+                                        <Card className="skill-card">
+                                            <Card.Img variant="top" src={skill.img} />
+                                            <Card.Body>
+                                                <Card.Title className="skill-title">{skill.title}</Card.Title>
+
+                                            </Card.Body>
+                                        </Card>
+                                    </Col>
+                                ))}
+                            </Row>
                         </div>
                     </Col>
                 </Row>
             </Container>
         </section>
-    )
-}
+    );
+};
